c3603056edbf966ce8fca0bc70ebf4c04f57fcde
alyssa cervantes <alyssac1003@gmail.com>
2021-03-08 09:11:52 -0800

    front page design

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+// import HomeView from '../components/HomeView.vue';
 import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,15 +1,45 @@
 <template>
-  <div class="home">
-    <HomeView msg="Welcome to Our App"/>
+  <div class="home" id="pic-back">
+    <div id="header">
+      <h1 class="display-1 font-weight-bold"> AniMe
+      </h1>
+      <h2> Explore and find new animes. </h2>
+    </div>
+    <HomeView/>
   </div>
 </template>
+
+<style>
+#header{
+  margin-top: 15%;
+  opacity: 1;
+  background-size: auto;
+}
+#pic-back::before{
+  top:0;
+  top: 0;
+  right: 0;
+  left: 0;
+  content: '';
+  background-image:url('~@/assets/anime_home_background.jpg');
+  height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  opacity: 0.3;
+  position: absolute;
+  z-index: -1;
+  width:100%;
+}
+
+</style>
 
 <script>
 // @ is an alias to /src
 import HomeView from '@/components/HomeView.vue';
 
 export default {
-  name: 'HomeView',
+  name: 'Home',
   components: {
     HomeView,
   },
