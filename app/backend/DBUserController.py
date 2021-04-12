5463920902051a6a9fd613fb1012050f2096b79d
jasonleh99 <jasonleh99@gmail.com>
2021-04-11 21:18:19 -0700

    Extended user with add and delete functions

--- app/backend/DBUserController.py
+++ app/backend/DBUserController.py
@@ -8,7 +8,6 @@ user_key = "Username"
 list_key = "AnimeList"
 type_key = "Type"
 email_key = "Email"
-settings_key = "Settings"
 # This class is used to help faciliate any interface functions relating to users
 # Mainly created to help make the logic more readable instead of stuffing DBController
 class DBUserController:
@@ -22,7 +21,7 @@ class DBUserController:
 
     # This is a helper method that will help build queries for filtering users
     # currently only built with the username / user_id in mind
-    def build_user_query(self, username=None, user_id=None):
+    def build_user_query(username=None, user_id=None):
         # if neither are provided, then we cannot query for whoever this function
         # is trying to find
         if username is None and user_id is None:
@@ -52,61 +51,23 @@ class DBUserController:
         self.get_users().update(query, update)
         return
 
-    # Adds a user to the database
-    def add_user(self, username, type, email, user_id=None):
+    def add_user(self, username, type, email):
         # modify user_id to be supplied in a better way to prevent clashes
-        id = random.randint(0, 999999999)
-        if user_id is not None:
-            id = user_id
+        user_id = randint()
 
         user = {
             user_key: username,
             list_key: [],
             type_key: type,
-            u_id: id,
+            u_id: user_id,
             email_key: email
         }
 
-        try:
-            self.get_users().insert_one(user)
-        except Exception as writeError:
-            print(writeError)
+        self.get_users().insert_one(user)
 
-    # Deletes a user from the database
     def delete_user(self, username=None, user_id=None):
         query = self.build_user_query(username=username, user_id=user_id)
         if query is None:
             return
 
-        try:
-            self.get_users().delete_one(query)
-        except Exception as opError:
-            print(opError)
-
-    def get_user_info(self, username=None, user_id=None):
-        query = self.build_user_query(username=username, user_id=user_id)
-        if query is None:
-            return
-
-        return self.get_users().find_one(query)
-
-    # For now we should not allow users to change their username / user_id
-    # Will update any present fields to whatever is provided
-    def update_user(self, username=None, user_id=None, email=None, anime_list=None, settings=None):
-        query = self.build_user_query(username=username, user_id=user_id)
-        if query is None:
-            return
-
-        changes = {}
-        if email is not None:
-            changes[email_key] = email
-        if anime_list is not None:
-            changes[list_key] = anime_list
-        if settings is not None:
-            changes[settings_key] = settings
-
-        update = {"$set": changes}
-        try:
-            self.get_users().update(query, update)
-        except Exception as writeError:
-            print(writeError)
+        self.get_users().delete_one(query)
